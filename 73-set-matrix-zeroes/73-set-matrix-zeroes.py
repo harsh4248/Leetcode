6411c39c52de4061c9a1afac8c779d4b2eeffba4
harsh4248 <58327992+harsh4248@users.noreply.github.com>
2022-09-21 13:23:47 -0400

    Time: 201 ms (59.08%), Space: 14.8 MB (16.70%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -1,38 +1,39 @@
 class Solution:
-    
+        
     def makeRowZero(self,i,matrix):
         for ir in range(len(matrix[i])):
             matrix[i][ir] = 0
     def makeColZero(self,j,matrix):
         for ij in range(len(matrix)):
             matrix[ij][j] = 0
-        pass
+    
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        # Step 1: copy matrix
-        cpyMat = []
-        for row in matrix:
-            temp = []
-            for colVal in row:
-                temp.append(colVal)
-            
-            cpyMat.append(temp)
-        
-        # cpyMat[0][0] = 100
-        # print(cpyMat,matrix)
-            
-        # Step 2: and solve question
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                if(cpyMat[i][j] == 0):
-                    self.makeRowZero(i,matrix)
-                    self.makeColZero(j,matrix)
-                
-        
-        
-        
-
- 
         """
         Do not return anything, modify matrix in-place instead.
         """
+        row = 0
+        col = 0
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if(matrix[i][j] == 0):
+                    if(i == 0):
+                        row = 1
+                    if(j == 0):
+                        col = 1
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        
+        for i in range(1,len(matrix)):
+            if(matrix[i][0] == 0):
+                self.makeRowZero(i,matrix)
+        for j in range(1,len(matrix[0])):
+            if(matrix[0][j] == 0):
+                self.makeColZero(j,matrix)
+        
+        if(row == 1):
+            self.makeRowZero(0,matrix)
+        if(col == 1):
+            self.makeColZero(0,matrix)
+        
+        
         
