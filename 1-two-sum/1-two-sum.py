1ca45c7956e3c186e38602abcf6b332d9e49ff6e
harsh4248 <58327992+harsh4248@users.noreply.github.com>
2022-10-03 07:37:37 -0400

    Time: 113 ms (63.47%), Space: 15.9 MB (8.93%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -3,9 +3,19 @@ class Solution:
         
 #         The most basic solution will be an brute approach
 #         using two for loops
+        # for i in range(len(nums)):
+        #     for j in range(len(nums)):
+        #         if nums[i] + nums[j] == target and i != j:
+        #             return [i,j]
+#         The above solution has time complexity of O(n^2)
+        numMap = {}
         for i in range(len(nums)):
-            for j in range(len(nums)):
-                if nums[i] + nums[j] == target and i != j:
-                    return [i,j]
+            if(target-nums[i] in numMap):
+                return [numMap[target-nums[i]][1],i]
+            else:
+                numMap[nums[i]] = (True,i)
+            
+                
+            
         
         
